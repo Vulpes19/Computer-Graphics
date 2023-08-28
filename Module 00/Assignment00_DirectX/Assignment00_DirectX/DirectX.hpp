d78a2abdefd22c4b037fcfd394de30de6c1fd2be
Vulpes19 <baioumyaymann@gmail.com>
2023-08-28 14:48:17 +0100

    Started the assignment using DirectX

---
+++ Module 00/Assignment00_DirectX/Assignment00_DirectX/DirectX.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+#include <Windowsx.h>
+#include <WinBase.h>
+#include <d3d11.h>
+#include <d3d10.h>
+#include <d3dcompiler.h>
+
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3d10.lib")
---
+++ Module 00/Assignment00_DirectX/Assignment00_DirectX/main.cpp
@@ -0,0 +1,74 @@
+#include "DirectX.hpp"
+
+LRESULT	CALLBACK	WindowProc(
+	HWND hWnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam
+)
+{
+	switch (message)
+	{
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return (0);
+		}
+		break;
+	}
+	return (DefWindowProcW(hWnd, message, wParam, lParam));
+}
+
+int	WINAPI	WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR		lpCmdLine,
+	int	nShowCmd
+)
+{
+	HWND	window;
+	WNDCLASSEX	windowClass;
+
+	ZeroMemory(&windowClass, sizeof(WNDCLASSEX));
+
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = WindowProc;
+	windowClass.hInstance = hInstance;
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	windowClass.lpszClassName = L"WindowClass1";
+	RegisterClassEx(&windowClass);
+
+	RECT size = { 0, 0, 800, 600 };
+	AdjustWindowRect(&size, WS_OVERLAPPEDWINDOW, FALSE);
+	
+	window = CreateWindowEx(
+		NULL,
+		L"WindowClass1",
+		L"Assignment00-DirectX",
+		WS_OVERLAPPEDWINDOW,
+		300,
+		300,
+		size.right - size.left,
+		size.bottom - size.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	ShowWindow(window, nShowCmd);
+	MSG msg = { 0 };
+
+	while (TRUE)
+	{
+		if (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessageW(&msg);
+			if (msg.message == WM_QUIT)
+				break;
+		}
+	}
+	return (msg.wParam);
+}
